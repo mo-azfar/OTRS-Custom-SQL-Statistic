e73acda5e334594aa97d4fdbde25df8e1fdfd2f4
Mohd Azfar <62957738+mo-azfar@users.noreply.github.com>
2020-09-03 16:31:29 +0800

    Update HandlingSQL6.pm

--- Kernel/System/Stats/Static/HandlingSQL6.pm
+++ Kernel/System/Stats/Static/HandlingSQL6.pm
@@ -17,13 +17,11 @@ use List::Util qw( first );
 use Kernel::System::VariableCheck qw(:all);
 use Kernel::Language qw(Translatable);
 
-use Time::Piece;
-
 our @ObjectDependencies = (
 	'Kernel::Config',
     'Kernel::Language',
     'Kernel::System::DB',
-    'Kernel::System::Time',
+    'Kernel::System::DateTime',
 	###to calling ticket attributes
 	'Kernel::System::Ticket',
 	###to calling ticket type attributes
@@ -61,6 +59,8 @@ sub GetObjectBehaviours {
 sub Param {
     my $Self = shift;
 	
+	my @Params = ();
+	
 	my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
 	 
 	###define ticket object
@@ -79,148 +79,156 @@ sub Param {
 	# my $BackendObject = $Kernel::OM->Get('Kernel::System::DynamicField::Backend');
 	# my $DynamicFieldObject = $Kernel::OM->Get('Kernel::System::DynamicField');
 	
-    # get time object
-    my $TimeObject = $Kernel::OM->Get('Kernel::System::Time');
-
-  
-	# get current time
-    my ( $s, $m, $h, $D, $M, $Y ) = $TimeObject->SystemTime2Date(
-        SystemTime => $TimeObject->SystemTime(),
+    # Get current time.
+    my $DateTimeObject = $Kernel::OM->Create(
+        'Kernel::System::DateTime',
     );
-	
+    my $DateTimeSettings = $DateTimeObject->Get();
+		
 	# get current year
-    my $SelectedYear  = $M == 1 ? $Y - 1 : $Y;
+    my $Y = sprintf( "%02d", $DateTimeSettings->{Year} );
 	# get one month before
-    my $SelectedMonth = $M == 1 ? 12     : $M - 1;
+    my $PM = sprintf( "%02d", $DateTimeSettings->{Month} - 1 );
 	# get current month
-	my $SelectedMonth2 = $M == 1 ? 12     : $M - 0;
-
-    # create possible time selections
-    my %Year = map { $_ => $_; } ( $Y - 10 .. $Y );
-    my %Month = map { $_ => sprintf( "%02d", $_ ); } ( 1 .. 12 );
-	my %ToMonth = map { $_ => sprintf( "%02d", $_ ); } ( 1 .. 12 );
+	my $M = sprintf( "%02d", $DateTimeSettings->{Month} );
+	
+	# Create possible time selections.
+    my %Year  = map { $_ => $_ } ( $Y - 10 .. $Y + 1 );
+    my %Month = map { sprintf( "%02d", $_ ) => sprintf( "%02d", $_ ) } ( 1 .. 12 );
+	
 	
 	###Get a list of ticket type
 	my %Ticket_Type = $TypeObject->TypeList(
             Valid => 1,
         );
 		
-	###Enable it to Get a list of queue	
-	# my %Ticket_Queue = $QueueObject->QueueList( 
-	# 		Valid => 1,	
-	#  	);	
+	##Enable it to Get a list of queue	
+	#  my %Ticket_Queue = $QueueObject->QueueList( 
+	#  		Valid => 1,	
+	#   	);	
 	
-	###Enable it to Get a list of customer user (might be depricated)
-	# my %List = $CustomerUserObject->CustomerUserList(
-    #     Valid => 1, # not required
-    # );
-		
+	##Enable it to Get a list of customer user (might be depricated)
+	#my %List = $CustomerUserObject->CustomerSearch(
+	#Search => '*',
+	#Valid => 1, # not required
+    #);
+	
+	
 	###Enable it to Get a specific ticket dynamic field (dropdown) parameter
-	# my $DFEngineer = $DynamicFieldObject->DynamicFieldGet(
-    #     Name => 'Engineer',
-    # );
-	# #get possible data of df engineer
-	# my $PossibleValues = $BackendObject->PossibleValuesGet(
-    #     DynamicFieldConfig => $DFEngineer,       
-    # );	
+	# my $DF1 = $DynamicFieldObject->DynamicFieldGet(
+    #      Name => 'Channel',
+    #  );
+	#  #get possible data of df engineer
+	#  my $PossibleValues1 = $BackendObject->PossibleValuesGet(
+    #      DynamicFieldConfig => $DF1,       
+    #  );	
 	
-	
-	my @Params = (
-	
-		{
-            Frontend   => 'Year',
-            Name       => 'Year',
-            Multiple   => 0,
-            Size       => 0,
-            SelectedID => $SelectedYear,
-            Data       => \%Year,
+	push @Params, {
+        Frontend   => 'From Month',
+        Name       => 'FromMonth',
+        Multiple   => 0,
+        Size       => 0,
+        SelectedID => $PM,
+        Data       => {
+            %Month, 
         },
-        {
-            Frontend   => 'From Month',
-            Name       => 'Month',
-            Multiple   => 0,
-            Size       => 0,
-            SelectedID => $SelectedMonth,
-            Data       => \%Month,
-        },
-		{
-            Frontend   => 'To Month',
-            Name       => 'ToMonth',
-            Multiple   => 0,
-            Size       => 0,
-            SelectedID => $SelectedMonth2,
-            Data       => \%ToMonth,
-        },
-		
-		###get selection field of type
-		{
-            Frontend   => 'Ticket Type',
-            Name       => 'Ticket_Type',
-            Multiple   => 1,
-            Size       => 0,
-            SelectedID => 1,
-			Data       => \%Ticket_Type,
-        },
-		
-		###Enable block to below to get selection field of queue
-		# {
-        #    Frontend   => 'Ticket Queue',
-        # 	Name       => 'Ticket_Queue',
-        # 	Multiple   => 1,
-        # 	Size       => 0,
-        # 	SelectedID => 1,
-        # 	Data       => \%Ticket_Queue,
-        # },
-		
-		###Enable block to below to get selection field of customer user
-		# {
-        # 	Frontend   => 'Customer User',
-        # 	Name       => 'CustomerUser',
-        # 	Multiple   => 1,
-        # 	Size       => 0,
-        # 	SelectedID => 1,
-        # 	Data       => \%List,
-        # },
-
-		###Enable block to below to get selection field of specific ticket dynamic field
-		# {
-        #     Frontend   => 'Engineer',
-        #     Name       => 'Engineer',
-        #     Multiple   => 1,
-        #     Size       => 0,
-        #     SelectedID => '-',
-        #     Data       => \%{$PossibleValues},
-        # },
-		
-		###For your own predefined dropdown/multiselect field
-		# {
-        # Frontend   => 'Company',
-        # Name       => 'Company',
-        # Multiple   => 1,
-        # Size       => 0,
-        # SelectedID => 0,
-        # Data       => {
-		# 			A => 'Site A',
-		# 			B => 'Site B',
-		# 			C => 'Site C',
-		# 			  },
-        # },
-		
-
-    );
-    return @Params;
+    };
 	
+	push @Params, {
+        Frontend   => 'To Month',
+        Name       => 'ToMonth',
+        Multiple   => 0,
+        Size       => 0,
+        SelectedID => $M,
+        Data       => {
+            %Month, 
+        },
+    };
 	
+	push @Params, {
+        Frontend   => 'For Year',
+        Name       => 'ForYear',
+        Multiple   => 0,
+        Size       => 0,
+        SelectedID => $Y,
+        Data       => {
+            %Year, 
+        },
+    };
+	
+	push @Params, {
+        Frontend   => 'Ticket Type',
+        Name       => 'Ticket_Type',
+        Multiple   => 1,
+        Size       => 0,
+        SelectedID => 1,
+        Data       => {
+           %Ticket_Type,
+        },
+    };
+	
+	###Enable block to below to get selection field of queue
+	# push @Params, {
+    #     Frontend   => 'Ticket Queue',
+    #     Name       => 'Ticket_Queue',
+    #     Multiple   => 1,
+    #     Size       => 0,
+    #     SelectedID => 1,
+    #     Data       => {
+    #       %Ticket_Queue,
+    #     },
+    # };
+	
+	##Enable block to below to get selection field of customer user
+	#push @Params, {
+    # 	Frontend   => 'Customer User',
+    # 	Name       => 'CustomerUser',
+    # 	Multiple   => 1,
+    # 	Size       => 0,
+    # 	SelectedID => 1,
+    # 	Data       => {
+	#		%List,
+    # 	},
+	#};
+	
+	###Enable block to below to get selection field of specific ticket dynamic field
+	# push @Params, {
+	# 	Frontend   => 'Channel',
+	# 	Name       => 'Channel',
+	# 	Multiple   => 1,
+	# 	Size       => 0,
+	# 	SelectedID => '-',
+    #  	Data       => {
+	# 		%{$PossibleValues1},
+    #  	},
+	# };
+	
+	###For your own predefined dropdown/multiselect field
+	#push @Params, {
+    # 	Frontend   => 'Company',
+    #   Name       => 'Company',
+    #   Multiple   => 1,
+    #   Size       => 0,
+    #   SelectedID => 0
+    # 	Data       => {
+	#		A => 'Site A',
+	# 		B => 'Site B',
+	# 		C => 'Site C',
+    # 	},
+	#};
+	
+	return @Params;
+	 
 }
 
 sub Run {
    my ( $Self, %Param ) = @_;
 
-    my $Year  = $Param{Year};
-    my $Month = sprintf( "%02d", $Param{Month});
-	my $ym = $Year.'-'.$Month.'-01'; 
+    my $ForYear  = $Param{ForYear};
+    my $FromMonth = sprintf( "%02d", $Param{FromMonth});
+	my $ym = $ForYear.'-'.$FromMonth.'-01'; 
 	my $ToMonth = sprintf( "%02d", $Param{ToMonth});
-	my $ytm = $Year.'-'.$ToMonth.'-31';
+	my $ytm = $ForYear.'-'.$ToMonth.'-31';
 	
 	#Some selected/submitted ticket attributes (E.g: type, queue) will contain an ID instead of name. 
 	#You need to using an id in SQL statement later
@@ -250,12 +258,12 @@ sub Run {
     # );
 	
     #for specific dynamic field
-	# my @engineer = $Param{Engineer};
-	# my $engineer2 = (${\(join "','", sort @{$engineer[0]})});
-	# my $engineer3 = $DBObject->QueryStringEscape(
-    # 			QueryString => $engineer2,
+	# my @Channel = $Param{Channel};
+	# my $Channel2 = (${\(join "','", sort @{$Channel[0]})});
+	# my $Channel3 = $DBObject->QueryStringEscape(
+    # 			QueryString => $Channel2,
     # );
-	
+			
 	#for predifined field above. Eg: company
 	# my @company = $Param{Company};
 	# my $company2 = (${\(join "','", sort @{$company[0]})});
@@ -263,36 +271,41 @@ sub Run {
     # 			QueryString => $company2,
     # );
 	
-	my $Title = "- Year $Year from Month $Month to $ToMonth"; 
+	my $Title = "- Year $ForYear from Month $FromMonth to $ToMonth"; 
 	my @Stats = ();
 
 	#example of sql. get handling time, 
 	my $SQL = "SELECT CONCAT_WS(' ', u.first_name,u.last_name) AS OWNER,
-t.tn AS OTRS_TICKET_NUMBER,
-t.title AS TITLE,
-q.name AS 'QUEUE',
-t.create_time AS OPEN_TIME,
-CASE WHEN th.history_type_id IN ('1','27') AND th.state_id IN ('2','3') THEN MAX(th.create_time) ELSE NULL END AS CLOSE_TIME,
-CASE WHEN th2.history_type_id IN ('8') THEN MIN(th2.create_time) ELSE NULL END AS AGENT_RESPONSE_TIME,
-tt.name AS 'TYPE',
-s.name AS 'SERVICE',
-sla.name AS 'SLA',
-ts.name AS 'STATE',
-t.customer_id AS CUSTOMER_ID,
-t.customer_user_id AS ALIAS
-FROM ticket t
-LEFT JOIN ticket_state ts ON (t.ticket_state_id=ts.id)
-LEFT JOIN queue q ON (t.queue_id=q.id)
-LEFT JOIN ticket_type tt ON (tt.id=t.type_id)
-LEFT JOIN service s ON (t.service_id=s.id)
-LEFT JOIN sla ON (t.sla_id=sla.id)
-LEFT JOIN users u ON (t.user_id=u.id)
-LEFT JOIN ticket_history th ON (t.id=th.ticket_id AND th.history_type_id IN ('1','27') AND th.state_id IN ('2','3'))
-LEFT JOIN ticket_history th2 ON (t.id=th2.ticket_id AND th2.history_type_id IN ('8'))
-LEFT JOIN customer_user cu ON (t.customer_user_id=cu.login)
-WHERE ( CAST(t.create_time AS DATE) BETWEEN ? AND ?)  AND tt.id IN ('$tt3')
-GROUP BY t.tn
-ORDER BY OWNER";
+	t.tn AS OTRS_TICKET_NUMBER,
+	t.title AS TITLE,
+	q.name AS 'QUEUE',
+	t.create_time AS OPEN_TIME,
+	CASE WHEN th.history_type_id IN ('1','27') AND th.state_id IN ('2','3') THEN MAX(th.create_time) ELSE NULL END AS CLOSE_TIME,
+	CONCAT(
+	TIMESTAMPDIFF(day,t.create_time,CASE WHEN th.history_type_id IN ('1','27') AND th.state_id IN ('2','3') THEN MAX(th.create_time) ELSE NULL END) , ' days ',
+	MOD( TIMESTAMPDIFF(hour,t.create_time,CASE WHEN th.history_type_id IN ('1','27') AND th.state_id IN ('2','3') THEN MAX(th.create_time) ELSE NULL END), 24), ' hours ',
+	MOD( TIMESTAMPDIFF(minute,t.create_time,CASE WHEN th.history_type_id IN ('1','27') AND th.state_id IN ('2','3') THEN MAX(th.create_time) ELSE NULL END), 60), ' minutes '
+	) AS HANDLING_TIME,
+	CASE WHEN th2.history_type_id IN ('8') THEN MIN(th2.create_time) ELSE NULL END AS AGENT_RESPONSE_TIME,
+	tt.name AS 'TYPE',
+	s.name AS 'SERVICE',
+	sla.name AS 'SLA',
+	ts.name AS 'STATE',
+	t.customer_id AS CUSTOMER_ID,
+	t.customer_user_id AS ALIAS
+	FROM ticket t
+	LEFT JOIN ticket_state ts ON (t.ticket_state_id=ts.id)
+	LEFT JOIN queue q ON (t.queue_id=q.id)
+	LEFT JOIN ticket_type tt ON (tt.id=t.type_id)
+	LEFT JOIN service s ON (t.service_id=s.id)
+	LEFT JOIN sla ON (t.sla_id=sla.id)
+	LEFT JOIN users u ON (t.user_id=u.id)
+	LEFT JOIN ticket_history th ON (t.id=th.ticket_id AND th.history_type_id IN ('1','27') AND th.state_id IN ('2','3'))
+	LEFT JOIN ticket_history th2 ON (t.id=th2.ticket_id AND th2.history_type_id IN ('8'))
+	LEFT JOIN customer_user cu ON (t.customer_user_id=cu.login)
+	WHERE ( CAST(t.create_time AS DATE) BETWEEN ? AND ?)  AND tt.id IN ('$tt3')
+	GROUP BY t.tn
+	ORDER BY OWNER";
 
 	$DBObject->Prepare( SQL => $SQL,
 	Bind => [ \$ym, \$ytm ]
